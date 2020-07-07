3e98c1cd216fd19ff58340839de0d24a9ac97c9f
ksitigarbha <58754415+karmicdribble@users.noreply.github.com>
2020-07-07 21:25:23 +0100

    added some tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,37 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+# check homepage for TD app
+        self.browser.get('http://localhost:8000')
+
+#check page title and header mention to do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+# invited to insert To do item
+
+# add to do item to text box
+
+# then page updates and now to do item is in a list
+
+# text box is still there inviting to add another item
+
+#page now updates and shows new item if second item is added
+
+#site generates unique URL for each user
+
+# if you return to that URL the user's stuff will still be here
+
+# end of User Story
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
