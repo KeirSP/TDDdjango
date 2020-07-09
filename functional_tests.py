537a2fc5e730ae95776c2cecdabff6bbe49149e4
ksitigarbha <58754415+karmicdribble@users.noreply.github.com>
2020-07-09 19:45:07 +0100

    added home template

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,15 +16,33 @@ class NewVisitorTest(unittest.TestCase):
 
 #check page title and header mention to do lists
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
 
 # invited to insert To do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
 # add to do item to text box
+        inputbox.send_keys('Buy peacock feathers')
 
 # then page updates and now to do item is in a list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find.elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
 # text box is still there inviting to add another item
+        self.fail('Finish the test!')
+
 
 #page now updates and shows new item if second item is added
 
